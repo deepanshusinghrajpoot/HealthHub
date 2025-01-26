daab09a7c85558e32353450896784ba0ce8e8e18
deepanshusinghrajpoot <deepanshusingh20022004@gmail.com>
2025-01-26 10:34:15 +0530

    production ready app

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,13 @@ import doctorRouter from "./routes/doctorRoute.js";
 import userRouter from "./routes/userRoute.js";
 
 
+//patel deploy
+import path from "path";
+
+
+
+
+
 // app config
 const app = express();
 const port = process.env.PORT || 4000;
@@ -19,10 +26,36 @@ app.use(express.json());
 app.use(cors());               // It is allow to connect the backend
 
 
+
+
+
+// patel deploy
+const _dirname = path.resolve();
+
+
+
+
+
+
 // api endpoints
 app.use("/api/admin", adminRouter );       //    localhost:4000/api/admin/add-doctor
 app.use("/api/doctor", doctorRouter);      //    
 app.use("/api/user", userRouter);
+
+
+
+
+
+
+//patel deploy 
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get("*", ( _ , res)=>{
+       res.sendFile(path.resolve(_dirname, "frontend", "dist",  "index.html"));
+})
+
+
+
+
 
 
 app.get("/", (req, res)=>{
