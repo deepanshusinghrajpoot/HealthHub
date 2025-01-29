f1e98eb5d516dd58a488d739c565f53d6b99cd39
deepanshusinghrajpoot <144131132+deepanshusinghrajpoot@users.noreply.github.com>
2025-01-29 12:08:09 +0530

    Update SpecialityMenu.jsx

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header } from './header'
+import { Header } from './Header'
 import { specialityData } from '../assets/assets'
 import { Link } from 'react-router-dom'
 
