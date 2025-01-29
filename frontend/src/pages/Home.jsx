edf439fc0667fa33bac638b6e0aca8e1ba62a074
deepanshusinghrajpoot <144131132+deepanshusinghrajpoot@users.noreply.github.com>
2025-01-29 12:09:36 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header } from '../components/header';
+import { Header } from '../components/Header';
 import { SpecialityMenu } from '../components/SpecialityMenu';
 import { TopDoctor } from '../components/TopDoctor';
 import { Banner } from '../components/Banner';
